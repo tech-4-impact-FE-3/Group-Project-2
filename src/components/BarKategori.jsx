8cff2c458e772970b30cd29692fbbf66f5c66d69
devaaditya28 <devaaditya28@gmail.com>
2022-11-17 16:24:56 +0700

    add component BarKategori

---
+++ src/components/BarKategori.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BarKategori = () => {
+    return (
+        <>
+        <div className="category">
+            <ul>
+                <li><a href="#">Trending</a></li>
+                <li><a href="#">Politics</a></li>
+                <li><a href="#">Health</a></li>
+                <li><a href="#">Education</a></li>
+                <li><a href="#">Others</a></li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default BarKategori;
