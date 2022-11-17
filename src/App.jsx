7bf52e1e01af1822be3fcc619e4bb351495dd118
anisahanisa <nisaanisah1206@gmail.com>
2022-11-17 21:43:23 +0700

    add components gambar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import BarKategori from './components/BarKategori';
+import Gambar from './components/gambar';
+import Gambarindex from './components/gambarIndex';
 import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      <BarKategori />
+    <Navbar /> 
+    <BarKategori />  
+    {/* <Gambar /> */}
+    <Gambarindex /> 
     </div>
   )
 }
---
+++ src/components/Gambar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import login from "..//img/login.png";
+
+const Gambar = () => {
+    return (
+        <div className = "login-img">
+        <img src={login} alt="img" />
+        </div>
+
+    )
+    
+}
+
+export default Gambar;
