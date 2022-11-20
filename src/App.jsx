6df1058c777ae9a5c519469e25800bf8f58f977e
devaaditya28 <devaaditya28@gmail.com>
2022-11-20 12:29:06 +0700

    add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Gambar from './components/gambar';
 import Gambarindex from './components/gambarIndex';
 import Navbar from "./components/Navbar";
 import BuatForum from './components/BuatForum';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
   return (
     <div className="App">
-    <Navbar /> 
-      <div className="d-flex justify-content-between">
-    <BarKategori />  
-    {/* <Gambar /> */}
-    {/* <Gambarindex />  */}
-      <BuatForum />
-      </div>
+      
+      {/* <Navbar /> 
+       <div className="d-flex justify-content-between">
+        <BarKategori />  
+        <Gambar />
+        <Gambarindex /> 
+        <BuatForum />
+      </div> */}
+      
+      <Dashboard />
     </div>
   )
 }
