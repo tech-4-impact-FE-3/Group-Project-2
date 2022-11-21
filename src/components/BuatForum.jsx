ad52f95b899231a8c65d434a09b9ec3078048663
Cynthia <cynthiaparamithaputri@gmail.com>
2022-11-22 00:21:13 +0700

    buat halaman upload dan atur fetch data

--- src/components/BuatForum.jsx
+++ src/components/BuatForum.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import buat from "../img/buat.png";
-import UploadForum from "../pages/UploadForum";
+import { Link } from "react-router-dom";
 
 const BuatForum = () => {
     return (
@@ -8,7 +8,7 @@ const BuatForum = () => {
             <div className="buat-forum card bg-transparent text-center border-0">
                 <img src={buat} alt="image-buat" className="card-img-top img-buat" />
                 <div className="card-body">
-                    <button type="button" className="buat-btn btn btn-primary">Buat Forum Baru</button>
+                <Link to={"/upload"}><button type="button" className="buat-btn btn btn-primary">Buat Forum Baru</button></Link>
                 </div>
             </div>
         </>
